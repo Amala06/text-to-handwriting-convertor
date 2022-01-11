1ea4d64bd1902c417afd3ebbf9b1b6ac4add4ac7
Amala Sebastian <90767720+Amala06@users.noreply.github.com>
2022-01-11 22:12:09 +0530

    Add files via upload

---
+++ hd.py
@@ -0,0 +1,13 @@
+import pywhatkit as kit
+text=input("enter the text :")
+last=input("enter the text: ")
+blast=input("enter the text: ")
+rain=input("enter the text: ")
+
+
+spain=input("enter the text: ")
+
+
+kit.text_to_handwriting(text+"\n"+last+"\n"+blast+"\n"+rain+"\n"+spain,rgb=[0,0,250])
+
+
